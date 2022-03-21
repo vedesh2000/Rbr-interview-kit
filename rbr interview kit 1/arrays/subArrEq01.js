2d0212eaf3c27a3289badf8620f619ba2ef0b4b6
vedesh2000 <vedeshkompella@gmail.com>
2022-03-21 12:58:58 +0530

    eq01

--- rbr interview kit 1/arrays/subArrEq01.js
+++ rbr interview kit 1/arrays/subArrEq01.js
@@ -1,42 +1,35 @@
 eq01SubArr = (arr) => {
     var n = arr.length;
+    var sum = 0;
+    var idx ;
+    var max = 0;
     var cur_sum = null;
     for (let i = 0; i < arr.length; i++) {
         if(arr[i] === 0)
         arr[i] = -1;        
     }
-    let start = 0;
-    let end = -1;
+    //console.log(arr);
     let hashMap = new Map();
-    for (let i = 0; i < n; i++) {
-        cur_sum = cur_sum + arr[i];
-        //check whether cur_sum - sum = 0, if 0 it means
-        //the sub array is starting from index 0- so stop
-        if (cur_sum === null) {
-            console.log("init");
-            start = 0;
-            end = i;
-            break;
+    for (let i = 0; i < arr.length; i++) {
+        sum = sum + arr[i];
+        if(hashMap.has(sum) === false){
+           // console.log("inserted " ,sum);
+            hashMap.set(sum , i);
         }
-        //if hashMap already has the value, means we already
-        // have subarray with the sum - so stop
-        if (hashMap.has(cur_sum)) {
-            start = hashMap.get(cur_sum) + 1;
-            end = i;
-            break;
+        else{
+            //console.log("else");
+            idx = hashMap.get(sum);
+            var length  = i - idx;
+            if(length > max){
+                max = length;
+                //console.log(max);
+            }
         }
-        //if value is not present then add to hashmap
-        hashMap.set(cur_sum, i);
 
+        
     }
-    if (end == -1) {
-        console.log("No subarray with given sum exists");
-    }
-    else {
-        console.log("Sum found between indexes "
-                        + start + " to " + end);
-    }
-    console.log(arr);
+   // console.log(hashMap);
+    return max;
 } 
 var x = eq01SubArr([1,1,1,0,1,0,0,0,1,1,1,0])
 console.log(x);
